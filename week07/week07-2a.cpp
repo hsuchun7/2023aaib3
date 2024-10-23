9b3899c0bdf2c6812d433d2afdf8e5af43824468
hsuchun7 <145088357+hsuchun7@users.noreply.github.com>
2024-10-23 11:50:38 +0800

    Add files via upload

---
+++ week07/week07-2a.cpp
@@ -0,0 +1,31 @@
+// week07-2A.cpp
+// Leetcode 1275
+class Solution {
+public:
+    void myDrawBoard(int board[3][3]){
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                cout << board[i][j] << ' ';
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+    }
+    bool testWin9(int board[3][3]){
+        if(board[0][0]==board[0][1] && board[0][1]==board[0][2]) return true;
+        if(board[1][0]==board[1][1] && board[1][1]==board[0][2]) return true;
+        if(board[2][0]==board[2][1] && board[2][1]==board[0][2]) return true;
+    }
+    string tictactoe(vector<vector<int>>& moves) {
+        int board[3][3]={};
+        int now = 1;
+        myDrawBoard(board);
+        for(auto move : moves){
+            int i = move[0], j = move[1];
+            board[i][j] = now;
+            myDrawBoard(board);
+            now = 3-now;
+        }
+        return  "B"; 
+    }
+};
