dbede0adba50d751c84283bbf5e8847ea9b49f5a
hsuchun7 <145088357+hsuchun7@users.noreply.github.com>
2024-12-11 11:47:51 +0800

    Add files via upload

---
+++ week2/week02-2.c
@@ -0,0 +1,13 @@
+//week02-2.c
+#include <stdio.h>
+
+int main()
+{
+	char s[200];
+	scanf("%s", s);
+	for(int i=0; s[i]!=0; i++){
+		char c = s[i];
+		if(s[i] != '2') printf("%c",c);
+	}
+	printf("\n");
+}
